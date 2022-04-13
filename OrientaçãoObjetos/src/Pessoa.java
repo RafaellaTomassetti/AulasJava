560e094dc9097d8e5587ef2869e556472c3267e4
aluno <aluno@DSN1251017048>
2022-04-13 11:13:54 -0300

    quarto commit

--- OrientaçãoObjetos/src/Pessoa.java
+++ OrientaçãoObjetos/src/Pessoa.java
@@ -8,12 +8,25 @@ public class Pessoa {
 		this.numFigurinhas  += numFigurinhas;
 	}
 	
-	void dar() {
+	boolean dar (int numFigurinhas, Pessoa pessoa) {
+	
 		
+		 if (numFigurinhas > this.numFigurinhas) { 
+		        System.out.println("Você não tem figurinhas suficientes para dar para " + pessoa.nome);
+		       return  false; // retorno booleano
+		        
+		        
+		     } else {
+		        System.out.println("Você tem figurinhas suficientes para dar para " + pessoa.nome);
+		    	this.numFigurinhas  -= numFigurinhas;
+				pessoa.receber(numFigurinhas);	
+				return true;   // retorno booleano
+		     }
+		 
+		 
+	
+				
 	}
-	
-	
-	
 	
 	
 	
--- OrientaçãoObjetos/src/main.java
+++ OrientaçãoObjetos/src/main.java
@@ -13,6 +13,23 @@ public class main {
 		System.out.println(pessoa1.nome);
 		System.out.println(pessoa1.numFigurinhas);
 
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.nome = "João";
+		pessoa2.numFigurinhas = 5;
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);
+
+		boolean msg = pessoa1.dar(3, pessoa2);
+		
+		if(msg == true) {
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);}
+
+		
 	}
 
 }
