f5d76fac2eeee90bfd96a2e833ae9bf4d54cd639
aluno <aluno@DSN1251230714>
2022-04-27 10:08:22 -0300

    commit

--- ModificadorAcesso/src/ContaBancaria.java
+++ ModificadorAcesso/src/ContaBancaria.java
@@ -5,15 +5,27 @@ public class ContaBancaria {
 	private boolean ativo;
 	private double saldo;
 
-	public void receber(double valor) { // esse método não retorna nenhuma informação
-		saldo += valor;
+	
+
+	public ContaBancaria() {
+		ativo=true;
+		saldo = 1.0;
+	}
+
+	public ContaBancaria(boolean ativo, double saldo) {
+		this.ativo = ativo;
+		this.saldo = saldo;
 	}
 
 	public double saldo() {
 		return saldo;
 
 	}
-
+	
+	public void receber(double valor) { // esse método não retorna nenhuma informação
+		saldo += valor;
+	}
+	
 	public void dar(double valor) {
 		
 		if (saldo >= valor)
--- ModificadorAcesso/src/main.java
+++ ModificadorAcesso/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 
-		ContaBancaria conta1 = new ContaBancaria(); // () = construtor
+		ContaBancaria conta1 = new ContaBancaria(true, 3.0); // () = construtor
 
 //		conta1.ativo = true;
 //		conta1.numConta = 1234;
@@ -14,12 +14,12 @@ public class main {
 //		System.out.println(conta1.numConta);
 //		System.out.println(conta1.saldo);
 
-		conta1.receber(100.8);
+		//conta1.receber(100.8);
 		conta1.dar(200);
 		System.out.println(conta1.saldo());
 		
 		ContaBancaria conta2 = new ContaBancaria();
-		conta2.receber(200.8);
+		//conta2.receber(200.8);
 		
 		System.out.println(conta2.saldo());
 
