014b2571a7b1cc974df0ec0710ec7f2f6424156e
aluno <aluno@DSN1251230714>
2022-05-02 10:18:36 -0300

    segundo commit

--- ModificadorAcesso/src/main.java
+++ ModificadorAcesso/src/main.java
@@ -14,22 +14,54 @@ public class main {
 //		System.out.println(conta1.numConta);
 //		System.out.println(conta1.saldo);
 
-		//conta1.receber(100.8);
+		// conta1.receber(100.8);
 		conta1.dar(200);
 		System.out.println(conta1.saldo());
-		
+
 		ContaBancaria conta2 = new ContaBancaria();
-		//conta2.receber(200.8);
-		
+		// conta2.receber(200.8);
+
 		System.out.println(conta2.saldo());
 
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("Rafael");
+		cliente1.setCpf(123456);
+		cliente1.setNumConta(77777);
+
+		System.out.println(cliente1.getNome());
+		System.out.println(cliente1.getCpf());
+		System.out.println(cliente1.getNumConta());
+
+		System.out.println(cliente1);
+
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Rafael");
+		cliente2.setCpf(123456);
+		cliente2.setNumConta(77777);
+
 		
+		System.out.println(cliente2);
+
+		if (cliente1.getNome() == cliente2.getNome()) { //compara o conteúdo de uma variável
+			System.out.println("Os nomes são iguais");
+		} else {
+			System.out.println("Os nomes são diferentes");
+
+		}
 		
-		
-		
-		
-		
-		
+		if(cliente1 == cliente2) { //compara os endereços de um objeto
+			System.out.println("Os endereços dos objetos são iguais");
+		} else {
+			System.out.println("Os endereços dos objetos são diferentes");
+
+		}
+
+		if(cliente1.equals(cliente2)) {    //compara os conteúdos dos objetos
+			System.out.println("Os atributos dos objetos são iguais");
+		} else {
+			System.out.println("Os atributos do objetos são diferentes");
+		}
+			
 		
 	}
 
