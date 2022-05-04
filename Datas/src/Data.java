c188ca1e970009af441c9c638e626f47f619bc21
aluno <aluno@DSN1251017048>
2022-05-04 09:37:15 -0300

    commit final

--- Datas/src/Data.java
+++ Datas/src/Data.java
@@ -1,93 +1,88 @@
 
 public class Data {
+	public static final int FORMATO_12H = 1;
+	public static final int FORMATO_24H = 2;
+
 	private int dia;
 	private int mes;
 	private int ano;
-	private int hora;
-	private int min;
-	private int seg;
+	private int hora = -1;
+	private int min = -1;
+	private int seg = -1;
 
+	// Construtor que recebe dia, mes e ano
 	public Data(int dia, int mes, int ano) {
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
+	}
+	public Data(int dia, int mes, int ano, int hora, int min, int seg) {
+		this(dia, mes, ano);
+		this.hora = hora;
+		this.min = min;
+		this.seg = seg;
+	}
+	
+	public void imprimir(int formato) {
+		String data = dia + "/" + mes + "/" + ano;
+				
+		if(hora == -1) {
+			System.out.println(data);
+		} else {
+			String horario = ":" + min + ":" + seg;
+				
+			if (formato == FORMATO_24H) {
+				horario = hora + horario;
+			} else {
+				// Se o formato for 12h
+				if (hora == 0) {
+					horario = 12 + horario;
+					horario += " AM";
+				} else if (hora >=12) {
+					// Se hora for maior ou igual a 12, é preciso subtrair 12 da hora para obter 
+					// a hora no formato 12h, e concatena o "PM" no fim
+					horario = (hora - 12) + horario;
+					horario += " PM";
+				} else {
+					// Se a hora for menor que 12, simplesmente utiliza a própria hora e concatena
+					// o "AM" no fim
+					horario = hora + horario;
+					horario += " AM";
 
+				}
+				
+			}
+			// Imprime a data/hora formatada
+						System.out.println(data + " " + horario);
+
+		}
 	}
 
 	public int getDia() {
 		return dia;
 	}
 
-	public void setDia(int dia) {
-		this.dia = dia;
-	}
 
 	public int getMes() {
 		return mes;
-	}
-
-	public void setMes(int mes) {
-		this.mes = mes;
 	}
 
 	public int getAno() {
 		return ano;
 	}
 
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
-
 	public int getHora() {
 		return hora;
-	}
-
-	public void setHora(int hora) {
-		this.hora = hora;
 	}
 
 	public int getMin() {
 		return min;
 	}
 
-	public void setMin(int min) {
-		this.min = min;
-	}
-
 	public int getSeg() {
 		return seg;
 	}
 
-	public void setSeg(int seg) {
-		this.seg = seg;
-	}
-
-	public Data(int dia, int mes, int ano, int hora, int min, int seg) {
-		this.dia = dia;
-		this.mes = mes;
-		this.ano = ano;
-		this.hora = hora;
-		this.min = min;
-		this.seg = seg;
-	}
-
-	public static final double FORMATO_12H;
-
-	static {
-		FORMATO_12H = (dia + "/" + mes + "/" + ano + "   " + hora + ":" + min + ":" + seg);
-	}
-	public static final double FORMATO_24H;
-
-	static {
-		FORMATO_24H =(dia + "/" + mes + "/" + ano + "   " + hora + ":" + min + ":" + seg);
-	}
-
-	public void imprimir(int dia, int mes, int ano, int hora, int min, int seg) {
-		System.out.println(dia + "/" + mes + "/" + ano + "   " + hora + ":" + min + ":" + seg);
-	}
-
-	public void imprimir(int dia, int mes, int ano) {
-		System.out.println(dia + "/" + mes + "/" + ano);
-	}
-
 }
+
+	
--- Datas/src/main.java
+++ Datas/src/main.java
@@ -1,16 +1,20 @@
-import java.util.Date;
-import java.text.SimpleDateFormat;
- 
 public class main {
    
     public static void main(String[] args)
     {
-        Data d1 = new Data(10, 05, 2005, 12, 56,23);
-        d1.imprimir(10, 05, 2005, 12, 56, 23);
+        Data d1 = new Data(01,02,03,04,05,06);
+        d1.imprimir(Data.FORMATO_24H);
+        d1.imprimir(Data.FORMATO_12H);
+        
        
         Data d2 = new Data(10, 06, 2009);
-        d2.imprimir(10, 06, 2009);
-      
+        d2.imprimir(Data.FORMATO_24H);
+        d2.imprimir(Data.FORMATO_12H);
+
+        Data d3 = new Data(20, 07, 2006);
+        d3.imprimir(Data.FORMATO_24H);
+        d3.imprimir(Data.FORMATO_12H);
+
    
     }
 }
