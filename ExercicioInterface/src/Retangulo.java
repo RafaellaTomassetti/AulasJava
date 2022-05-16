c16cc6c6d123c627f8b2af241611c03beeba82dd
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-16 07:48:10 -0300

    Update Retangulo.java
    comentários feitos

--- ExercicioInterface/src/Retangulo.java
+++ ExercicioInterface/src/Retangulo.java
@@ -1,15 +1,15 @@
 
-public class Retangulo implements AreaCalculavel {
+public class Retangulo implements AreaCalculavel { //classe pública que implementa a interface AreaCalculavel
 
-	int base = 30;
+	int base = 30;  //atributos necessários para calcular a área do retângulo
 	int altura = 20;
 	
 	@Override
-	public void calcularArea() {
+	public void calcularArea() {   //override do método calcular área da interface.
 		
-		int area;
-		area = base*altura;
-		System.out.println("A �rea do ret�ngulo � " + area + " cm2");
+		int area;   //variável criada para guardar o valor da área
+		area = base*altura;    //fórmula para cálculo da área do quadrado
+		System.out.println("A área do retângulo é " + area + " cm2");
 
 	}
 
