8950d8df9de6936f50a3b54e6c2861105f934d0e
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-16 08:03:50 -0300

    Update ContaCorrente.java
    comentários feitos

--- Exercicio2Interface/src/ContaCorrente.java
+++ Exercicio2Interface/src/ContaCorrente.java
@@ -1,11 +1,13 @@
 
-public class ContaCorrente extends ContaBancaria{
+public class ContaCorrente extends ContaBancaria{ //classe pública que herda a classe-mãe ContaBancaria
 
-	@Override
-	public double calcularSaldo() {
-		double saldofinal;
-		saldofinal = getSaldo()*0.90; //saldo menos 10%
-		System.out.println("Seu saldo final na Conta Corrente � R$ " + saldofinal);
+	@Override  
+	public double calcularSaldo() {    //override do método calcular saldo da superclasse.
+		double saldofinal;     //variável criada para guardar o valor do saldo final
+		
+		saldofinal = getSaldo()*0.90; //fórmula para calcular o saldo menos 10%
+		
+		System.out.println("Seu saldo final na Conta Corrente é R$ " + saldofinal);
 		return saldofinal;
 	}
 
