d98d2f33daa802dc0dfcc9e6cfc94a90c7178cbe
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-16 08:01:38 -0300

    Update Aplicacao.java
    comentários feitos

--- Exercicio2Interface/src/Aplicacao.java
+++ Exercicio2Interface/src/Aplicacao.java
@@ -1,20 +1,20 @@
 
-public class Aplicacao {
+public class Aplicacao { //classe main para aplicação dos métodos
 
 	public static void main(String[] args) {
 
-		ContaCorrente contacorrente = new ContaCorrente();
-		contacorrente.depositar(200);
-		contacorrente.transferir(350);
-		contacorrente.sacar(1000);
-		contacorrente.calcularSaldo();
-		System.out.println();
+		ContaCorrente contacorrente = new ContaCorrente(); //objeto contacorrente instanciado. É da classe ContaCorrente
+		contacorrente.depositar(200);    //depositar 200 reais no saldo da contacorrente
+		contacorrente.transferir(350);   //transferir 350 reais no saldo da contacorrente
+		contacorrente.sacar(1000);       //sacar 200 reais no saldo da contacorrente
+		contacorrente.calcularSaldo();   //calcular saldo final
+		System.out.println();            //pular linha
 		
 		ContaInvestimento containvestimento = new ContaInvestimento();
-		containvestimento.depositar(30);
-		containvestimento.transferir(40);
-		containvestimento.sacar(20);
-		containvestimento.calcularSaldo();
+		containvestimento.depositar(30);     //depositar 200 reais no saldo da containvestimento
+		containvestimento.transferir(40);    //transferir 40 reais no saldo da containvestimento
+		containvestimento.sacar(20);         //sacar 200 reais no saldo da containvestimento
+		containvestimento.calcularSaldo();   //calcular saldo final
 
 		
 	}
