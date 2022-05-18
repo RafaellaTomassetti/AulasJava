b57efbeef100ba662631d1b5427b78bad2196b1c
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-18 09:01:41 -0300

    Add files via upload
    commit novo

---
+++ FundamentosJava/src/main.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		// switch case ou escolha caso
+		// int num = 10;
+
+		// switch (num) {
+
+		// case 1:
+//			System.out.println("Domingo");
+		// break;
+		// case 2:
+		// System.out.println("Segunda");
+		// break;
+		// case 3:
+		// System.out.println("Terça");
+		// break;
+		// case 4:
+		// System.out.println("Quarta");
+		// break;
+		// case 5:
+		// System.out.println("Quinta");
+		// break;
+		// case 6:
+		// System.out.println("Sexta");
+		// break;
+		// case 7:
+		// System.out.println("Sábado");
+//			break;
+//		default:
+//			System.out.println("Este número não representa um dia da semana");
+		// }
+//		char caractere = 'd';
+//
+//		switch (caractere) {
+//		case 'a':
+//			System.out.println("Sua letra é A");
+		// break;
+//		case 'b':
+		// System.out.println("Sua letra é B");
+//			break;
+//		case 'c':
+		// System.out.println("Sua letra é C");
+		// break;
+
+//		default:
+//			System.out.println("Valor inválido");
+
+		// }
+		// }
+
+		// enquanto while
+		//
+		// int x = 0;
+		// while(x < 10) {
+		//
+		// System.out.println(x);
+		// x = x + 1;
+
+		// }
+
+		// x=0;
+		// do {
+		// System.out.println(x);
+		// x = x + 1;
+		// }while(x<10);
+
+//}
+
+//}
+
+		for (int x = 0; x < 10; x++) {
+			System.out.println(x);
+		}
+
+		for (int x = 0; x <= 10; x++)
+		
+		{
+			System.out.println("2 x" + x + " = " + x * 2);
+		}
+		Scanner entrada =  new Scanner (System.in);
+		
+		entrada.nextInt();
+		System.out.println("Digite o valor que deseja multiplicar:");
+	}
+}
