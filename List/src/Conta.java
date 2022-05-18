77027e36379ab802ac83ef41c8bd4f8324277014
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-18 11:36:29 -0300

    Add files via upload

---
+++ List/src/Conta.java
@@ -0,0 +1,18 @@
+
+public class Conta {
+	String nome;
+	double valor;
+
+	@Override
+	public String toString() {
+		return "Conta [nome=" + nome + ", valor= R$ " + valor + "]";
+	}
+
+	public Conta(String nome, double valor) {
+		this.nome = nome;
+		this.valor = valor;
+
+	}
+
+}
+
