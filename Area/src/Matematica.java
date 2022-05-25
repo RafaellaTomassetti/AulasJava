e00fea5622df9b8dd8bee88a4fd523a7aa99cf0f
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-25 10:12:50 -0300

    Update Matematica.java
    comentários

--- Area/src/Matematica.java
+++ Area/src/Matematica.java
@@ -1,20 +1,15 @@
-
-public class Matematica {
-
-	int calculararea(int x) { //declarando método para calcular a área com o parâmetro x (o lado)
-
-		return x * x;
-	}
-        //declarando método para calcular a área com o parâmetro x e y(a base e a altura)
-	double calculararea(double x, double y) {
-
-		return (x * y) / 2;
-	}
+public class matematica {
 	
-        //declarando método para calcular a área com o parâmetro x e y (a base e a altura)
+	// declarando método para calcular a área com o parâmetro x (o lado)
+	int calculararea(int x) { 
+		return x * x; //retornará a área do quadrado
+	}
+
+	// declarando método para calcular a área com o parâmetro x e y (a base e a
+	// altura)
 	int calculararea(int x, int y) {
 
-		return x * y;
+		return x * y;  //retornará a área do retângulo
 	}
 
 }
