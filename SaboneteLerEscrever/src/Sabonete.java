0b6c3f2e4a70aa6ef191006bec8644699a0b1ee9
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-25 11:36:05 -0300

    Add files via upload
    comentario

---
+++ SaboneteLerEscrever/src/Sabonete.java
@@ -0,0 +1,34 @@
+
+public class Sabonete {
+	
+	private String cheiro; //atributos privados da classe Sabonete
+	private String cor;
+
+
+	// toString para retornar todos os atributos dos sabonetes
+	@Override
+	public String toString() {
+		return "Sabonete [cheiro=" + cheiro + ", cor=" + cor + "]\n";
+	}
+	
+	//construtor que define os parâmetros para instanciar objetos
+	public Sabonete(String cheiro, String cor) {
+		super();
+		this.cheiro = cheiro;
+		this.cor = cor;
+	}
+	
+	public Sabonete(String texto) {
+		String[] arrayAuxiliar1 = texto.split(",");
+		String[] arrayAuxiliar2 = arrayAuxiliar1[0].split("=");
+		this.cheiro = arrayAuxiliar2[1].trim();
+
+		String[] arrayAuxiliar3 = arrayAuxiliar1[1].split("=");
+		this.cor = arrayAuxiliar2[1].trim();
+
+	}
+
+}
+	
+	
+
