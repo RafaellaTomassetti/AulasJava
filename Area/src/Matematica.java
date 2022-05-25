f5731d0535e6b38f1ca06bf55ffc20de6cdaeffa
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-25 10:05:36 -0300

    Update Matematica.java
    comentado

--- Area/src/Matematica.java
+++ Area/src/Matematica.java
@@ -1,16 +1,17 @@
 
 public class Matematica {
 
-	int calculararea(int x) {
+	int calculararea(int x) { //declarando método para calcular a área com o parâmetro x (o lado)
 
 		return x * x;
 	}
-
+        //declarando método para calcular a área com o parâmetro x e y(a base e a altura)
 	double calculararea(double x, double y) {
 
 		return (x * y) / 2;
 	}
-
+	
+        //declarando método para calcular a área com o parâmetro x e y (a base e a altura)
 	int calculararea(int x, int y) {
 
 		return x * y;
