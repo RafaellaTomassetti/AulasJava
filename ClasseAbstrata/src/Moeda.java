a2facbd5593ebbebe8851d448898d352b08eed07
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 10:05:10 -0300

    Update Moeda.java

--- ClasseAbstrata/src/Moeda.java
+++ ClasseAbstrata/src/Moeda.java
@@ -1,9 +1,10 @@
 
-public class Moeda extends Item {
+public class Moeda extends Item { //subclasse qu extende de ITEM
 
+	//Override do método pegar da classe-mãe
 	@Override
 	public void pegar() {
-		System.out.println("Pegou a Moeda +50 pontos");
+		System.out.println("Pegou a Moeda +50 pontos"); //mensagem que aparece ao usar o método
 	}
 	
 
