0c3365621ad9c44d7947207cbb7eb98cf9fc820b
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 10:32:59 -0300

    Update main.java

--- EscreverTXT/src/main.java
+++ EscreverTXT/src/main.java
@@ -11,21 +11,26 @@ public class main {
 List<Sabonete> listaDeSabonete = new ArrayList<Sabonete>(); //fazendo uma lista da classe Sabonete 
 		
 		Sabonete sabonete = new Sabonete(" lavanda"," roxo"); //instanciando um objeto sabonete
-		listaDeSabonete.add(sabonete);
+		listaDeSabonete.add(sabonete);  //adicionando objeto na lista
 		
 		Sabonete sabonete2 = new Sabonete(" baunilha"," amarelo"); //instanciando um objeto sabonete
-		listaDeSabonete.add(sabonete2);
+		listaDeSabonete.add(sabonete2); //adicionando objeto na lista
 
 		
 		Sabonete sabonete3 = new Sabonete(" rosas"," rosa"); //instanciando um objeto sabonete
-		listaDeSabonete.add(sabonete3);
+		listaDeSabonete.add(sabonete3); //adicionando objeto na lista
 		
+		//Buffer é uma memória temporária que pode ler ou escrever 
+		//o try verifica se a ação será realizada completamente
 		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){ //essa linha deve estar num TRY para caso aconteca algum erro no caminho
-			for (Sabonete sabonetes : listaDeSabonete) {				//se deu errado a situação, ele mostra o erro 
+												//se deu errado a situação, ele mostra o erro 
 		//junta as informações e escreve onde precisa
 		//o lugar(destino) onde a informação será inscrita é o parâmetro
 		//criação do objeto responsável pela escrita
 		//se não tiver o arquivo, ele cria e depois escreve
+			
+			for (Sabonete sabonetes : listaDeSabonete) {				
+	
 		
 		escrever.write(sabonetes.toString()); //write é um método que escreve
 		//write recebe a variável que contém o texto
