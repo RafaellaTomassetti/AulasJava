24ca1fc3c81374a16ec625107dfa9efd3f7ee519
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 08:25:53 -0300

    Update Diamante.java

--- ClasseAbstrata/src/Diamante.java
+++ ClasseAbstrata/src/Diamante.java
@@ -1,8 +1,9 @@
 
-public class Diamante extends Item {
+public class Diamante extends Item {  //subclasse qu extende de ITEM
 
+	//Override do método pegar da classe-mãe
 	@Override
 	public void pegar() {
-		System.out.println("Pegou o Diamante +200 pontos");
+		System.out.println("Pegou o Diamante +200 pontos");  //mensagem que aparece ao usar o método
 	}
 }
