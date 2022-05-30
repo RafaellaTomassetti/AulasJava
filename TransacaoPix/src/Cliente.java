024f14e73ceffb9917f8fa3cf5518953d997627a
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 09:19:32 -0300

    Update Cliente.java

--- TransacaoPix/src/Cliente.java
+++ TransacaoPix/src/Cliente.java
@@ -1,19 +1,19 @@
 
-public class Cliente {
-	String nome;
+public class Cliente {  //classe pública
+	String nome;    //atributos priavdos do Cliente
 	int conta;
 	Pix pix; 
 	Double valor;
 	
 	
-	void receber(Double valor) {  //método com parâmetro de valor
-		this.valor += valor;
+	void receber(Double valor) {  //método receber com parâmetro de valor
+		this.valor += valor;   //pega o valor inicial e recebe um outro
 	}
 
-	boolean dar(Double valor, Cliente cliente) {
+	boolean dar(Double valor, Cliente cliente) {//método dar com parâmetro de valor e o cliente que vai receber
 
-		if (valor > this.valor) {
-			System.out.println("Você não tem valor suficientes para transferir para " + cliente.nome);
+		if (valor > this.valor) {  //condição lógica para saber se tem valor suficiente na conta para transferir
+			System.out.println("Você não tem valor suficientes para transferir para " + cliente.nome);  //
 			System.out.println("Seu saldo é " + this.valor);
 			return false; // retorno booleano
 
