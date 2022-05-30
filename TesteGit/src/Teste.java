0d216f91efcf133cc2b7ffd4a4caaeb89aab009c
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 10:34:24 -0300

    Update Teste.java

--- TesteGit/src/Teste.java
+++ TesteGit/src/Teste.java
@@ -1,8 +1,8 @@
 
-public class Teste {
+public class Teste {  //CLASSE PÚBLICA
 
-	public static void main(String[] args) {
-		System.out.println("Rafaella");
+	public static void main(String[] args) { 
+		System.out.println("Rafaella");  //MOSTRAR INFORMAÇÃO NA TELA DO USUÁRIO
 	}
 
 }
