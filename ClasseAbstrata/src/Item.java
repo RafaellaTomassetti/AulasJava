5fdfb1cbcb36ffaed1bfba43a3307bddd241f89e
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 08:24:52 -0300

    Update Item.java

--- ClasseAbstrata/src/Item.java
+++ ClasseAbstrata/src/Item.java
@@ -1,11 +1,11 @@
 
 public abstract class Item { //classe abstrata
 	
-	private int posicaoX = 1;
+	private int posicaoX = 1;  //atributos privados
 	private int posicaoY = 1;
 	
-	public abstract void pegar();  //todos que herdarem a classe ITEM, devem ter o método pegar
-	//classe abstrata só tem a assinatura (;)
+	public abstract void pegar();  //todos que herdarem a classe ITEM, devem ter o mÃ©todo pegar
+	//classe abstrata sÃ³ tem a assinatura (;)
 	//se fosse implementar, teriam as chaves ({})
 	
 	
