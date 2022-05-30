c61f45c71cbf832e09a2e85db1e0ca26af56a0d4
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 08:47:58 -0300

    Update Conta.java

--- List/src/Conta.java
+++ List/src/Conta.java
@@ -3,11 +3,13 @@ public class Conta { //classe pública
 	String nome; //atributos da classe
 	double valor;
 
+	//toString para retornar os atributos
 	@Override
 	public String toString() {
 		return "Conta [nome=" + nome + ", valor= R$ " + valor + "]";
 	}
-
+	
+	//construtor que cria objetos com os atributos pré-definidos
 	public Conta(String nome, double valor) {
 		this.nome = nome;
 		this.valor = valor;
