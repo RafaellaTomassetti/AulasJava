39036b77461b6f2faf5c8e5ccdb174216181f62d
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 10:21:45 -0300

    Update main.java

--- ExercicioInterface/src/main.java
+++ ExercicioInterface/src/main.java
@@ -4,13 +4,13 @@ public class main {  //classe main para aplicação dos métodos
 	public static void main(String[] args) {
 	
 		AreaCalculavel circulo = new Circulo();  //objeto círculo instanciado. É da classe AreaCalculavel
-		circulo.calcularArea();
+		circulo.calcularArea();  //usando o método de calcular área do CÍRCULO
 		
 		AreaCalculavel quadrado = new Quadrado(); //objeto quadrado instanciado. É da classe AreaCalculavel
-		quadrado.calcularArea();	
+		quadrado.calcularArea();	//usando o método de calcular área do QUADRADO
 		
 		AreaCalculavel retangulo = new Retangulo(); //objeto retângulo instanciado. É da classe AreaCalculavel
-		retangulo.calcularArea();
+		retangulo.calcularArea();      //usando o método de calcular área do RETÂNGULO
 		
 		
 	}
