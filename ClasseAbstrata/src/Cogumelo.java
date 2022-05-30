78924daf9bc5286206cf0fb36931662f03a6a040
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 10:04:01 -0300

    Update Cogumelo.java

--- ClasseAbstrata/src/Cogumelo.java
+++ ClasseAbstrata/src/Cogumelo.java
@@ -1,9 +1,11 @@
 
-public class Cogumelo extends Item {
+public class Cogumelo extends Item { //subclasse qu extende de ITEM
 
+	
+	//Override do método pegar da classe-mãe
 	@Override
 	public void pegar() {
-		System.out.println("Pegou o Cogumelo +100 pontos");
+		System.out.println("Pegou o Cogumelo +100 pontos"); //mensagem que aparece ao usar o método
 	}
 	
 
