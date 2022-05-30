e7f3dc0e55eb7dfa4775f39f36611bd22d42a53b
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-05-30 10:16:09 -0300

    Update Aplicacao.java

--- ClasseAbstrata/src/Aplicacao.java
+++ ClasseAbstrata/src/Aplicacao.java
@@ -1,18 +1,18 @@
 
-public class Aplicacao {
+public class Aplicacao {  //CLASSE MAIN 
 
 	public static void main(String[] args) {
 		
-		// Item item = new Item();  //n�o pode criar uma inst�ncia de uma classe abtrata
+		// Item item = new Item();  //não pode criar uma instância de uma classe abtrata
 	
-		Item item = new Cogumelo();  //criou a vari�vel
-		item.pegar();
+		Item item = new Cogumelo();  //Instanciou objeto  CRIOU UMA VARIÁVEL
+		item.pegar();  //realizou ação pela classe COGUMELO
 		
-		item = new Moeda();      //vari�vel j� criada, n�o precisa repetir o nome da classe (Item)
-		item.pegar();
+		item = new Moeda();      //variável já criada, não precisa repetir o nome da classe (Item)
+		item.pegar(); //realizou ação pela classe MOEDA
 		
-		item = new Diamante();
-		item.pegar();
+		item = new Diamante(); //variável já criada, não precisa repetir o nome da classe (Item)
+		item.pegar(); //realizou ação pela classe DIAMANTE
 		
 		
 		
