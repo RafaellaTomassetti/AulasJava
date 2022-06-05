ac7624f0f4b84a2ad4466f4ca8c09744bc0cde92
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 13:07:55 -0300

    Update Matematica.java

--- ElementosStaticos/src/Matematica.java
+++ ElementosStaticos/src/Matematica.java
@@ -2,23 +2,14 @@
 public class Matematica {
 // classe que vai subtrair e somar 2 números
 
-	public static int somar(int a, int b) { //método static somar, não precisa instanciar um objeto
-		return a + b;  //retorna a soma
+	public static int somar(int a, int b) { // método static somar, não precisa instanciar um objeto
+		return a + b; // retorna a soma
 
 	}
 
-	public static int subtrair(int a, int b) { //método static somar, não precisa instanciar um objeto
-		return a - b; //retorna a subtração
+	public static int subtrair(int a, int b) { // método static subtrair, não precisa instanciar um objeto
+		return a - b; // retorna a subtração
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
