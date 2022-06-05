255db1ffecf7e873f3a9c5d1cc3b2aa7c81da5e6
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 13:21:02 -0300

    Update Constante.java

--- ElementosStaticos/src/Constante.java
+++ ElementosStaticos/src/Constante.java
@@ -2,10 +2,11 @@
 public class Constante {
 //ninguém consegue mudá-lo, mesmo com a visibilidade pública
 	
-	public static final double MEDIA_DA_PROVA;
+	// final em Java serve para atribuir valores constantes, ou seja,
+	//que a partir da sua declaração, seus valores não poderão mais serem alterados.
+	public static final double MEDIA_DA_PROVA; 
 	
 	//static não precisa criar objetos pra acessar os métodos
-	
 	static {
 		MEDIA_DA_PROVA = 7.0; //definindo uma constante
 	}
