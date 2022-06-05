c747f9debd5c56d9e4f2cc3b10a1e611c3fbcec5
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 20:12:34 -0300

    Update Moto.java

--- Heranca/src/Moto.java
+++ Heranca/src/Moto.java
@@ -1,8 +1,9 @@
 
-public class Moto extends Veiculo{
+public class Moto extends Veiculo{ //classe-filha que estende da classe-mãe Veículo
 
-	public void empinar() {
-		System.out.println("UHUUUULL");
+	public void empinar() {  //Método novo que apenas a moto tem.
+		//mas a moto possui os outros métodos da classe mãe.
+		System.out.println("UHUUUULL"); //mostrando texto na tela
 		
 	}
 
