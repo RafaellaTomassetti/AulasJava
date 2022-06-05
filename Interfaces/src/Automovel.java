41bb19987e907ef8b4c4c1bdee7afed27ed48303
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 20:40:19 -0300

    Update Automovel.java

--- Interfaces/src/Automovel.java
+++ Interfaces/src/Automovel.java
@@ -1,7 +1,11 @@
 
-public interface Automovel {
+public interface Automovel {  
+	//interfaces não têm atributos e tem apenas assinaturas de métodos
+	//cabe à classe que a utiliza, immplementar as assinaturas
 
-	void virarDireita();
+	//assinaturas dos métodos da interface
+	//as classes que implementam a interface podem incrementar as assinaturas
+	void virarDireita();  
 	void virarEsquerda();
 	void acelerar();
 	
