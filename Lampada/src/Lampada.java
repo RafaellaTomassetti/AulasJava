6f394203ba05d8df5ebfc07230ef3822ded39c85
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 16:31:41 -0300

    Update Lampada.java

--- Lampada/src/Lampada.java
+++ Lampada/src/Lampada.java
@@ -3,7 +3,7 @@ public class Lampada { //classe pública Lampada
 	private boolean estado;  //variável booleana privada
 	
 
-	public Lampada() {
+	public Lampada() { //estado inicial da lâmpada é true
 		this.estado = true;
 	}
 
@@ -12,13 +12,13 @@ public class Lampada { //classe pública Lampada
 
 	}
 
-	public void desligar() {
+	public void desligar() {  //método desligar tem um retorno falso
 		estado = false;
 
 	}
 
 	public void imprimir() {
-		if (estado == true) {
+		if (estado == true) {  //condição lógica para analisar se o estado da lâmpada é true ou não
 			System.out.println("A lâmpada está ligada");
 			
 		} else {
