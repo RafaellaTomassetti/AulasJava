4f1284deabdd077de9715c794b3ec0bc72a0a80f
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:28:01 -0300

    Update main.java

--- Polimorfismo/src/main.java
+++ Polimorfismo/src/main.java
@@ -3,26 +3,26 @@ public class main {
 
 	public static void main(String[] args) {
 
-//		Animal animal = new Cachorro();    //cachorro est· com "extends" em animal. 
-//		                                  //A vari·vel animal est· apontando para o objeto "Cachorro"
+//		Animal animal = new Cachorro();    //cachorro est√° com "extends" em animal. 
+//		                                  //A vari√°vel animal est√° apontando para o objeto "Cachorro"
 //		animal.falar();
 //
-//		animal = new Gato();            //gato est· endereÁado em "animal"
+//		animal = new Gato();            //gato est√° endere√ßado em "animal"
 //		animal.falar();
-		// isso sÛ acontece porque a superclasse È animal
+		// isso s√≥ acontece porque a superclasse √© animal
 
-		Gato gato = new Gato();  //criei um objeto tipo gato na heap. A vari·vel "gato" aponta para a classe Gato.
-		Cachorro cachorro = new Cachorro(); //criei um objeto tipo cachorro na heap. A vari·vel "cachorro" aponta para a classe Cachorro.
-		falar(gato);             //o mÈtodo falar precisa de um par‚metro dentro de "Animal"
+		Gato gato = new Gato();  //criei um objeto tipo gato na heap. A vari√°vel "gato" aponta para a classe Gato.
+		Cachorro cachorro = new Cachorro(); //criei um objeto tipo cachorro na heap. A vari√°vel "cachorro" aponta para a classe Cachorro.
+		falar(gato);             //o m√©todo falar precisa de um par√¢metro dentro de "Animal"
 		falar(cachorro);
 
 	}
 
-	public static void falar(Animal a) {     //o "a" È a inst‚ncia. O "a" representa um animal. Nesse caso ele aponta para o "gato"
+	public static void falar(Animal a) {     //o "a" √© a inst√¢ncia. O "a" representa um animal. Nesse caso ele aponta para o "gato"
 	a.Falar();
 		
-	if(a instanceof Cachorro) {         // instanceof È um operador de comparaÁ„o. A resposta se· true ou false.
-	          						//ele vÍ se o objeto È da classe que eu estou comparando.
+	if(a instanceof Cachorro) {         // instanceof √© um operador de compara√ß√£o. A resposta ser√° true ou false.
+	          						//ele v√™ se o objeto √© da classe que eu estou comparando.
 		   Cachorro c = (Cachorro) a;  
 		   c.Morder();
 		
