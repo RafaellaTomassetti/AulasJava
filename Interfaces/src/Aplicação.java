34e22954dd35a230754c2d704f41d4c317ebafe3
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:07:38 -0300

    Update Aplicação.java

--- Interfaces/src/Aplicação.java
+++ Interfaces/src/Aplicação.java
@@ -1,26 +1,26 @@
 
 public class Aplicação {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  //classe main para executar o programa
 		
-		Rota rota = new Rota();          //instanciando objeto
-		Ferrari ferrari = new Ferrari(); //instanciando objeto
-		Fusca fusca = new Fusca();       //instanciando objeto
+		Rota rota = new Rota();          //instanciando objeto da classe Rota
+		Ferrari ferrari = new Ferrari(); //instanciando objeto da classe Ferrari
+		Fusca fusca = new Fusca();       //instanciando objeto da calsse Fusca
 		
-		rota.ir(ferrari);
+		rota.ir(ferrari);  //aplicando métodos nos objetos
 		rota.ir(fusca);
 		
 		
-		Automovel automovel = new Ferrari();
-		rota.ir(automovel);
+		Automovel automovel = new Ferrari();  //instaciando objeto da classe Ferrari
+		rota.ir(automovel); //aplicando métodos no objeto
 		
 		automovel = new Fusca();
 		rota.ir(automovel);
 		
-		Vendedor vendedor = new Vendedor();
-		Apartamento apartamento = new Apartamento();
+		Vendedor vendedor = new Vendedor();  //instaciando objeto da classe Vendedor
+		Apartamento apartamento = new Apartamento();   //instaciando objeto da classe Apartamento
 		
-		vendedor.mostrarPreco(apartamento);
+		vendedor.mostrarPreco(apartamento);  //aplicando métodos nos objetos
 		vendedor.mostrarPreco(ferrari);
 
 	}
