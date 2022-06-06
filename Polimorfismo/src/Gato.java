fa727de769f7c5bce6e8897c10f21616aa49ea1c
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:26:54 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -1,6 +1,8 @@
 
-public class Gato extends Animal{
-
+public class Gato extends Animal{ //classe Cachorro que estende de Animal
+//Gato é a classe-filha
+	
+	//sobrescrita do método falar da classe-mãe
 	@Override
 	public void Falar() {
 		System.out.println("Miaau");
