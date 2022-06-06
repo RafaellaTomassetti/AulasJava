06f684d2e40d55e3e1de4b14b09ea53b7eda7f15
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:25:48 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,10 +1,13 @@
 
-public class Cachorro extends Animal{
-
+public class Cachorro extends Animal{  //classe Cachorro que estende de Animal
+	//Cachorro éa classe-filha
+	
+	//sobrescrita do método falar da classe-mãe
 	@Override
 	public void Falar() {
 		System.out.println("Au Au");
 	}
+	//criação  de um método que só o chachorro tem
 	public void Morder() {
 		System.out.println("NHAAAAC");
 	}
