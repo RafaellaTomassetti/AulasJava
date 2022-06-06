5953bda7dc4bdce040adc591940478b3a6d2addc
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:42:26 -0300

    Update Aplicação.java

--- PolimorfismoBebidas/src/Aplicação.java
+++ PolimorfismoBebidas/src/Aplicação.java
@@ -6,10 +6,10 @@ public class Aplicação {
 		Preparador preparador = new Preparador(); //instanciando objeto da classe Preparador
 		
 		LeiteQuente leiteQuente = new LeiteQuente(); //instanciando objeto da classe LeiteQuente
-		preparador.prepararBebida(leiteQuente);        //preparando a bebida leite quente
+		preparador.prepararBebida(leiteQuente);         //preparando a bebida leiteQuente com o método prepararBebida da classe preparador
 		
 		Refrigerante refrigerante = new Refrigerante(); //instanciando objeto da classe Refrigerante
-		preparador.prepararBebida(refrigerante);      //preparando a bebida refrigerante
+		preparador.prepararBebida(refrigerante);      //preparando a bebida refrigerante com o método prepararBebida da classe preparador
 		
 	}
 
