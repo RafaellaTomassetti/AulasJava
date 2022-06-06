a06680c01c6030b2f7e2937ffe5aca4e0020e747
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:01:18 -0300

    Update Apartamento.java

--- Interfaces/src/Apartamento.java
+++ Interfaces/src/Apartamento.java
@@ -1,9 +1,10 @@
 
-public class Apartamento implements ItemCaro{
+public class Apartamento implements ItemCaro{  //classe Apartamento que implementa ItemCaro
 
+	//sobreescrita do atributo da interface ItemCaro
 	@Override
 	public double getPreco() {
-		return 250000;
+		return 250000;  //retorno do valor
 	}
 
 	
