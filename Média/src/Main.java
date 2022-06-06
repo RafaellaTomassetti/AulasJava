03678b9a0e4365ccb1a9463744c3110824b9e517
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-05 21:13:31 -0300

    Update Main.java

--- Média/src/Main.java
+++ Média/src/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Media notas = new Media(); // criando a variável e instanciando o objeto pessoa
-		notas.setNota1(8.5);
+		Media notas = new Media(); // criando a variável e instanciando o objeto notas
+		notas.setNota1(8.5);  //utilizando o set para atribuir um valor a variável
 		notas.setNota2(6.4);
 		notas.setNota2(9.5);
 
-		notas.imprimir();
+		notas.imprimir(); //utilizando o método no objeto criado
 		
 	}
 
